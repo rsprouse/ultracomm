5016b34a331906f6b330356b10114b04fcd40e2d
Ronald Sprouse <ronald@berkeley.edu>
2014-07-31 10:25:19 -0700

    Revert "Change exit codes to #define constants."
    This reverts commit b83a2c8bafaff8f8538b2d4ddd0296697ec956f9.

--- src/exit_codes.h
+++ src/exit_codes.h
@@ -1,11 +1,6 @@
-//  One or more required command line parameters are missing.
-#define MISSING_REQUIRED_OPTION_ERROR 1
+const int MISSING_REQUIRED_OPTION_ERROR = 1;
+const int MISSING_OPTIONS_FILE_ERROR = 2;
 
-// An options file was identified on the command line but can't be accessed.
-#define MISSING_OPTIONS_FILE_ERROR 2
+const int CONNECTION_ERROR = 100;
 
-// Could not connect to the Ultrasonix server.
-#define CONNECTION_ERROR 100
-
-// Unknown.
-#define UNKNOWN_ERROR 1000
+const int UNKNOWN_ERROR = 1000;
